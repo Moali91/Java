de7fedb426b55f132402a46ff738c40afeac0b0e
Mohammed Ali <mohammed.ali313@hotmail.com>
2019-10-05 16:46:08 +0200

    Online Statics added

--- src/dataTypes/OnlineStatistics.java
+++ src/dataTypes/OnlineStatistics.java
@@ -1,4 +1,41 @@
 package dataTypes;
 
 public class OnlineStatistics {
+
+    private int sum=0;
+    private int valuesNum = 0;
+    private int min = Integer.MAX_VALUE;
+    private int max = Integer.MIN_VALUE;
+
+
+    public void addValue(int value){
+        sum += value;
+        valuesNum++;
+        min = Math.min(min,value);
+        max = Math.max(max,value);
+    }
+
+    public int getSum(){
+        return sum;
+    }
+
+    public double getAverage(){
+        return ((double)sum)/valuesNum;
+    }
+
+    public int getMax(){
+        return max;
+    }
+
+    public int getMin(){
+        return min;
+    }
+
+    public void reset(){
+        sum =0;
+        valuesNum =0;Y
+        min = Integer.MAX_VALUE;
+        max = Integer.MIN_VALUE;
+    }
+
 }
